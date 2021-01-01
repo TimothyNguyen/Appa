93aa45d03ecf873c254db976f99bfe09d85b23f7
Timothy Nguyen <quynhthoa1972@gmail.com>
2021-01-01 17:17:17 -0500

    New info

--- resume-builder/index.js
+++ resume-builder/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 
 require('dotenv').config();
-
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,4 +10,4 @@ app.use(express.json());
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
-});
+}); 
