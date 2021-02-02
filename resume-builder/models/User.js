c9dc27a7367d969dd6968ca6bb37e1e8d8ca149a
Timothy Nguyen <quynhthoa1972@gmail.com>
2021-02-02 11:20:39 -0500

    Fix small issues with docker and setting up REDIS_URL

--- resume-builder/models/User.js
+++ resume-builder/models/User.js
@@ -16,8 +16,58 @@ type User struct {
 }
 */
 
+const { ObjectID } = require('mongodb');
 const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
 
 const { Schema } = mongoose;
 
+const UserSchema = new Schema({
+    _id: {
+        type: ObjectID,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    phone_number: {
+        type: String,
+    },
+    github_username: {
+        type: String,
+    },
+    linkedin_link: {
+        type: String,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    verified: {
+        type: Boolean,
+        default: false,
+    },
+    verification_url_code: {
+        type: String,
+        default: () => uuidv4(),
+    },
+    password_reset_url_code: {
+        type: String,
+        default: '',
+    },
+    user_type: {
+        type: String,
+    },
+})
+
+const User = mongoose.model('users', UserSchema);
+module.exports = User;
